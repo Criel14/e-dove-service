95184ec0c523e3aa3e3f63a1b8e98fc811172f22
criel <chencriel@qq.com>
2025-09-26 20:27:41 +0800

    fix: 注册用户信息拦截器，使其生效

---
+++ e-dove-common/src/main/java/com/criel/edove/common/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.criel.edove.common.config;
+
+import com.criel.edove.common.interceptor.UserInfoContextInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Web配置：注册拦截器
+ */
+@Configuration
+@EnableWebMvc
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final UserInfoContextInterceptor userInfoContextInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userInfoContextInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/error",
+                        "/static/**",
+                        "/css/**",
+                        "/js/**",
+                        "/images/**",
+                        "/webjars/**"
+                );
+    }
+}
