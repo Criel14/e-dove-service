509c84990a44daa21607cd1eb6ceebec31a06272
criel <chencriel@qq.com>
2025-09-29 22:18:40 +0800

    refactor: 修改异常枚举信息

--- e-dove-common/src/main/java/com/criel/edove/common/exception/ErrorCode.java
+++ e-dove-common/src/main/java/com/criel/edove/common/exception/ErrorCode.java
@@ -4,31 +4,36 @@ import lombok.Getter;
 
 /**
  * 自定义错误码和错误信息
- * 1xxx：通用错误
- * 2xxx：用户服务错误
+ * 1xxx：通用异常
+ * 2xxx：auth服务异常
+ * 3xxx：user服务异常
  */
 @Getter
 public enum ErrorCode {
 
+    // 通用异常
     SYSTEM_ERROR("1001", "系统异常，请联系管理员"),
 
+    // auth服务异常
     JWT_ERROR("2001", "用户信息校验异常"),
     REFRESH_TOKEN_ERROR("2002","用户信息校验异常"),
     USER_NOT_FOUND("2003","用户不存在"),
-    LOGIN_PHONE_OTP_ERROR("2004","手机验证码错误"),
-    PASSWORD_NOT_FOUND("2005","该用户未设置密码"),
-    PASSWORD_ERROR("2006","密码错误"),
-    LOGIN_MISSING_PARAMETER("2007","登录参数缺失"),
+    PASSWORD_NOT_FOUND("2004","该用户未设置密码"),
+    PASSWORD_ERROR("2005","密码错误"),
+    LOGIN_MISSING_PARAMETER("2006","登录参数缺失"),
+    LOGIN_PHONE_OTP_ERROR("2007","手机验证码错误"),
     REGISTER_MISSING_PARAMETER("2008","注册参数缺失"),
     PHONE_ALREADY_EXISTS("2009","手机号已被注册"),
     EMAIL_ALREADY_EXISTS("2010","邮箱已被注册"),
-    REGISTER_PHONE_OTP_ERROR("2011","手机验证码错误"),
-    REGISTER_EMAIL_OTP_ERROR("2012","邮箱验证码错误"),
-    OTP_PARAMETER_ERROR("2013","手机号或邮箱格式错误"),
-    OTP_MISSING_PARAMETER("2014","验证码请求参数缺失"),
-    USERNAME_ALREADY_EXISTS("2015","用户名已被注册"),
-    USERINFO_MISSING_USER_ID("2016","用户信息缺失用户ID"),
-    OTP_REQUEST_TOO_FREQUENTLY("2017","验证码请求频率过快"),;
+    USERNAME_ALREADY_EXISTS("2011","用户名已被注册"),
+    REGISTER_PHONE_OTP_ERROR("2012","手机验证码错误"),
+    REGISTER_EMAIL_OTP_ERROR("2013","邮箱验证码错误"),
+    OTP_PARAMETER_ERROR("2014","手机号或邮箱格式错误"),
+    OTP_MISSING_PARAMETER("2015","验证码请求参数缺失"),
+    OTP_REQUEST_TOO_FREQUENTLY("2016","验证码请求频率过快"),
+
+    // user服务异常
+    USERINFO_MISSING_USER_ID("3001","用户信息缺失用户ID"),;
 
     private final String code;
     private final String message;
