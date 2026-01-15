4944e2224615d7a97a5fe0f6879b1681c8bba21d
criel <chencriel@qq.com>
2026-01-15 18:05:54 +0800

    feat: 新增assistant模块

---
+++ e-dove-assistant/src/main/java/com/criel/edove/assistant/EDoveAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.criel.edove.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDoveAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EDoveAssistantApplication.class, args);
+    }
+
+}
---
+++ e-dove-assistant/src/main/java/com/criel/edove/assistant/controller/AssistantController.java
@@ -0,0 +1,17 @@
+package com.criel.edove.assistant.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 大模型调用 Controller
+ *
+ * @author Criel
+ * @since 2026-01-15
+ */
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/assistant")
+public class AssistantController {
+}
