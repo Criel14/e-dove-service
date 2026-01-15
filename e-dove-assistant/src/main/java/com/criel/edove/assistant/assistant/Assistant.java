5c9620634ffd0d88819b17fb75a7155eb70e493d
criel <chencriel@qq.com>
2026-01-15 23:21:16 +0800

    fix: 修复了大模型输出的格式问题

--- e-dove-assistant/src/main/java/com/criel/edove/assistant/assistant/Assistant.java
+++ e-dove-assistant/src/main/java/com/criel/edove/assistant/assistant/Assistant.java
@@ -15,14 +15,14 @@ public interface Assistant {
     @UserMessage("""
             请根据下面的省市区，随机生成{{count}}个标准的详细地址。
             省：{{province}}，市：{{city}}，区/县：{{district}}
-            输出内容只包含地址，不带额外解释，以JSON格式输出
+            输出内容只包含地址，不带额外解释，以JSON格式输出，示例：
+            ["百合园小区6栋701", "金园路102号"]
             """)
-    List<String> generateAddress(
+    String generateAddress(
             @V("count") Integer count,
             @V("province") String province,
             @V("city") String city,
             @V("district") String district
     );
-
 
 }
--- e-dove-assistant/src/main/java/com/criel/edove/assistant/service/impl/AssistantServiceImpl.java
+++ e-dove-assistant/src/main/java/com/criel/edove/assistant/service/impl/AssistantServiceImpl.java
@@ -1,10 +1,16 @@
 package com.criel.edove.assistant.service.impl;
 
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONUtil;
 import com.criel.edove.assistant.assistant.Assistant;
 import com.criel.edove.assistant.dto.AddressGenerateDTO;
 import com.criel.edove.assistant.service.AssistantService;
 import com.criel.edove.assistant.vo.AddressGenerateVO;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,12 +34,14 @@ public class AssistantServiceImpl implements AssistantService {
     @Override
     public AddressGenerateVO generateAddresses(AddressGenerateDTO addressGenerateDTO) {
         // 调用大模型生成地址数组
-        List<String> addresses = assistant.generateAddress(
+        String result = assistant.generateAddress(
                 addressGenerateDTO.getCount(),
                 addressGenerateDTO.getStoreAddrProvince(),
                 addressGenerateDTO.getStoreAddrCity(),
                 addressGenerateDTO.getStoreAddrDistrict()
         );
+        JSONArray jsonArray = JSONUtil.parseArray(result);
+        List<String> addresses = jsonArray.toList(String.class);
         return new AddressGenerateVO(addresses);
     }
 
