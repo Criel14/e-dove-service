b9d2507fec3b806337dd9867ff94052a0804febc
criel <chencriel@qq.com>
2026-01-24 13:56:21 +0800

    fix: 修复了远程调用方法出错的问题

--- e-dove-feign/src/main/java/com/criel/edove/feign/auth/client/AuthFeignClient.java
+++ e-dove-feign/src/main/java/com/criel/edove/feign/auth/client/AuthFeignClient.java
@@ -6,10 +6,7 @@ import com.criel.edove.common.result.Result;
 import com.criel.edove.common.vo.PingVO;
 import com.criel.edove.feign.auth.dto.UpdateUserAuthDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * e-dove-auth模块的远程调用
@@ -27,7 +24,7 @@ public interface AuthFeignClient {
      * 更新用户认证信息接口：仅支持修改：用户名 和 邮箱
      * 仅远程调用：需要确保在e-dove-user中已经验证邮箱的验证码，验证用户名是否能在
      */
-    @PostMapping("/auth/update")
+    @PutMapping("/auth/update")
     Result<Object> update(@RequestBody UpdateUserAuthDTO updateUserAuthDTO);
 
 }
